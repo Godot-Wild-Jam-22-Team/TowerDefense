36c851e9f69d2769b72eedd61a779eb973277ad6
gioele <gioele.santi@gmail.com>
2020-06-15 15:22:11 +0200

    Update Vision.gd
    Converted to typed script

--- GWJ22/Defenses/Turrets/Vision.gd
+++ GWJ22/Defenses/Turrets/Vision.gd
@@ -5,22 +5,22 @@ class_name Vision
 signal updated
 signal exited
 
-var enemy
+var enemy: KinematicBody2D
 var enemy_pos := Vector2(0,0)
 var has_focus := false
 
-func _on_Vision_body_entered(body):
+func _on_Vision_body_entered(body: KinematicBody2D) -> void:
 	if(has_focus == false):
 		has_focus = true
 		enemy = body
 
-func _on_Vision_body_exited(body):
+func _on_Vision_body_exited(body: KinematicBody2D) -> void:
 	has_focus = false
 	emit_signal('exited')
 
 func update_enemy_position() -> void:
 	if(has_focus == true):
-		emit_signal('updated', enemy.position)
+		emit_signal('updated', enemy.global_position)
 
 func _process(_delta: float) -> void:
 	update_enemy_position()
