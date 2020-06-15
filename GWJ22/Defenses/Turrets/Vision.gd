0303b22fcf24e7a12ea5b037b6eea039e3c3be76
gioele <gioele.santi@gmail.com>
2020-06-15 13:12:32 +0200

    Set vision as scene node fo turret
    This way it is easier to manage and Game does not know about internal workings of turret

--- GWJ22/Defenses/Turrets/Vision.gd
+++ GWJ22/Defenses/Turrets/Vision.gd
@@ -1,13 +1,15 @@
 extends Area2D
 
+class_name Vision
+
 signal updated
 signal exited
 
 var enemy
-var enemy_pos = Vector2(0,0)
-var has_focus = false
+var enemy_pos := Vector2(0,0)
+var has_focus := false
 
-func start(_position, _direction):
+func start(_position : Vector2, _direction: Vector2):
 	position = _position
 	rotation = _direction.angle()
 
@@ -20,9 +22,9 @@ func _on_Vision_body_exited(body):
 	has_focus = false
 	emit_signal('exited')
 
-func update_enemy_position():
+func update_enemy_position() -> void:
 	if(has_focus == true):
 		emit_signal('updated', enemy.position)
 
-func _process(delta):
+func _process(_delta: float) -> void:
 	update_enemy_position()
