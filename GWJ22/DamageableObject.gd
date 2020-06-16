db03964ed4627650f9a07f84684f94c9367c2d51
gioele <gioele.santi@gmail.com>
2020-06-16 22:37:18 +0200

    Adjusted foe attack logic
    It has to be tested in battle to see if it approaches and attacks without problems (especially once enemy is dead).

--- GWJ22/DamageableObject.gd
+++ GWJ22/DamageableObject.gd
@@ -9,11 +9,16 @@ var health := 4 setget set_health # 10 for
 func take_damage(value: int = 1) -> void:
 	health -= value
 
+func die() -> void:
+	emit_signal("die", self.name)
+	queue_free()
+
 func set_health(value) -> void:
 	health = max(0, value)
 	emit_signal("health_changed", health)
 	if health <= 0:
-		emit_signal("die", self.name)
+		die()
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
--- GWJ22/Foes/Foe.gd
+++ GWJ22/Foes/Foe.gd
@@ -2,14 +2,15 @@ extends DamageableObject
 
 class_name Foe
 
+enum State {IDLE, WALK, ATTACK, DIE}
+var current_state = State.IDLE setget set_current_state
+
 onready var animation_player := $AnimationPlayer
 
 # Movement
 export (float, 50.0, 200.0) var speed := 100.0
 var velocity := Vector2.ZERO
-var path := PoolVector2Array()
-
-var go_to_point:= Vector2.ZERO
+var path := []
 
 # Attack
 export (int, 1, 5) var ATTACK_POWER := 1
@@ -28,45 +29,66 @@ func initialize(start_position: Vector2, destination: Vector2) -> void:
 	# a pivot where to turn and a couple of points in between to variate the path
 	
 	path.append(destination)
-	go_to_point = path[0]
+	self.current_state = State.WALK
 
 
 func _process(delta: float) -> void:
-	if global_position.distance_to(go_to_point) < distance_threshold:
-		go_to_point = next_point()
-	
-	velocity = go_to_point - global_position
+	var temp_destination := Vector2.ZERO
+	match current_state:
+		State.WALK:
+			if path.size() > 0 and global_position.distance_to(path[0]) < distance_threshold:
+				path.remove(0)
+			if path.size() <= 0:
+				self.current_state = State.IDLE
+				return
+			temp_destination = path[0]
+			
+		State.ATTACK:
+			temp_destination = targets[0].global_position
+			if global_position.distance_to(temp_destination) < distance_threshold:
+				attack(targets[0]) #add a tempo for attack
+				return #do not walk after attack (especially if the target dies)
+			
+	velocity = temp_destination - global_position
 	global_position += velocity.normalized() * speed * delta
-
-func next_point() -> Vector2:
-	if targets.size() > 0:
-		#iterate to find the nearest one
-		var target : Area2D = targets[0]
-		return target.global_position
-	
-	if path.size() > 1:
-		path.remove(0)
-		return path[0]
-	
-	return path[0]
 
 # //should also react to damage, so listen for health change signal(?) - I cannot know the direction as bullets are indpeendent objects, unless I follow the incoming direction
 
 # bullets should implement an attack method too when touching an area in foe group
 
 func attack(target: DamageableObject) -> void:
+	#bounce animation
 	target.take_damage(ATTACK_POWER)
 
 func _on_View_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
 		targets.append(area)
-		go_to_point = targets[0]
+		self.current_state = State.ATTACK
 
 func _on_View_area_exited(area: Area2D) -> void:
 	if area.is_in_group("player"):
 		var idx = targets.find(area)
 		targets.remove(idx)
-		if targets.size() > 0:
-			go_to_point = targets[0]
-		elif path.size() > 0 :
-			go_to_point = path[0]
+		if targets.size() <= 0:
+			self.current_state = State.WALK
+
+func die():
+	self.current_state = State.DIE
+	.die() #I could need to call this at the end of death animation
+
+func set_current_state(new_state) -> void:
+	if new_state == current_state:
+		return
+	
+	current_state = new_state
+	
+	match current_state:
+		State.WALK:
+			set_process(true)
+		State.ATTACK:
+			set_process(true)
+		State.IDLE:
+			set_process(false)
+		State.DIE:
+			set_process(false)
+			pass #any animation here
