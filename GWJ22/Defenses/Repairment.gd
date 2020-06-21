eb4c4820ce7a72514d5b468350b116313afe6b0d
gioele <gioele.santi@gmail.com>
2020-06-21 02:00:54 +0200

    Update Repairment.gd
    Inverted area reaction. I will leave this part as rest of game transitioned to physics bodies instead of area2d, so this will need a lot of rework to work properly  on everything

--- GWJ22/Defenses/Repairment.gd
+++ GWJ22/Defenses/Repairment.gd
@@ -1,16 +1,14 @@
 extends DraggableObject
 
+func _set_droppable(value: bool) -> void:
+	#print("Changed droppable %s" % value)
+	_droppable = value
+	if not _grabbed:
+		return
+	if not _droppable:
+		_reset_flash()
+	else:
+		_start_flash()
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
+func _get_droppable() -> bool:
+	return not _droppable
