2f5c3acf95f5982d01e011592435c936dcad2250
gioele <gioele.santi@gmail.com>
2020-06-21 18:36:13 +0200

    Update Vision.gd
    Now turret points the nearest enemy

--- GWJ22/Defenses/Turrets/Vision.gd
+++ GWJ22/Defenses/Turrets/Vision.gd
@@ -5,6 +5,7 @@ class_name Vision
 signal updated(position)
 signal reset()
 
+var enemy_distances = Array()
 var enemy_positions = Array()
 
 func _on_Vision_body_entered(body: KinematicBody2D) -> void:
@@ -16,8 +17,10 @@ func _process(_delta: float) -> void:
 	
 	var candidates = get_overlapping_bodies()
 	enemy_positions = []
+	enemy_distances = []
 	for body in candidates:
 		if body.is_in_group("foes"):
+			enemy_distances.append(global_position.distance_to(body.global_position))
 			enemy_positions.append(body.global_position)
 	
 	if enemy_positions.size() <= 0:
@@ -25,5 +28,6 @@ func _process(_delta: float) -> void:
 		emit_signal('reset')
 		return
 	else:
-		#sort
-		emit_signal('updated', enemy_positions[0])
+		var min_distance = enemy_distances.min()
+		var index = enemy_distances.find(min_distance)
+		emit_signal('updated', enemy_positions[index])
